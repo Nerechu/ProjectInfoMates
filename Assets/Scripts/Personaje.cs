55ccaa9be05d8534e4d9cf64e51cd6620e874741
Nerea <nerea.alon41@gmail.com>
2022-11-20 22:34:14 +0100

    Código con animaciones

--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class Personaje : MonoBehaviour
 {
     public float _vel;
+    private float _velocidadCaminar;
 
     public GameObject _prefabProjectil;
     public GameObject _posCano;
 
+    private AmimacionesPersonaje _animacionesPersonaje;
+    private SpriteRenderer _spriteRendererPersonaje;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _velocidadCaminar = 5f;
+
+        _animacionesPersonaje = GetComponent<AmimacionesPersonaje>();
+        _spriteRendererPersonaje = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -39,12 +46,28 @@ public class Personaje : MonoBehaviour
 
         Vector2 dirrecioIndicada = new Vector2(direccioX, direccioY).normalized;
 
-        //Trobem posició actual del personatge:
+        //Trobem posiciï¿½ actual del personatge:
         Vector2 posNova = transform.position;
         posNova += dirrecioIndicada * _vel * Time.deltaTime;
         posNova.x = Mathf.Clamp (posNova.x, minPantalla.x, maxPantalla.x);
         posNova.y = Mathf.Clamp(posNova.y, minPantalla.y, maxPantalla.y);
 
         transform.position = posNova;
+
+        float inputHorizontal = Input.GetAxisRaw("Horizontal") * _velocidadCaminar;
+        OrientacionSprite(inputHorizontal);
+        _animacionesPersonaje.MovimientoHorizontal(inputHorizontal);
+    }
+
+    void OrientacionSprite(float inputHorizontal){
+        if (inputHorizontal > 0){
+
+            _spriteRendererPersonaje.flipX = false;
+
+        } else if(inputHorizontal < 0) {
+
+            _spriteRendererPersonaje.flipX = true;
+            
+        }
     }
 }
